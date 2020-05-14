d14a691c7505eea3b6e53292b8a67f379acfa924
Laurent Eriksen <laurent.eriksen@loepfe.com>
2020-05-14 22:43:50 +0200

    Solve remove k digits problem

---
+++ src/RemoveKDigits.cpp
@@ -0,0 +1,41 @@
+#include "RemoveKDigits.h"
+
+namespace RemoveKDigits
+{
+	std::string Solution::removeKdigits(std::string num, int k) {
+		if (k == num.length())
+		{
+			return "0";
+		}
+		int i{ 0 };
+		while ((i < num.length() - 1) && k > 0)
+		{
+			if (num[i] <= num[i + 1])
+			{
+				i++;
+			}
+			else
+			{
+				k--;
+				num.erase(num.begin() + i);
+				if (i > 0)
+				{
+					i--;
+				}
+			}
+			while (num[0] == '0')
+			{
+				num.erase(num.begin());
+			}
+		}
+		if (k > 0)
+		{
+			num.erase(num.begin() + (num.length() - k), num.end());
+		}
+		if (num == "")
+		{
+			return "0";
+		}
+		return num;
+	}
+}
--- src/SingleElementsInASortedArray.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <vector>
-
-class Solution {
-public:
-	int singleNonDuplicate(std::vector<int>& nums);
-};
