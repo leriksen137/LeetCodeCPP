3a02abd34b7091d6b35cbeb765e2650dd7bb3e0d
Laurent Eriksen <laurent.eriksen@loepfe.com>
2020-05-17 10:17:11 +0200

    Solve FindAllAnagramsInAString

---
+++ Exercises/FindAllAnagramsInAString.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <string>
+#include <vector>
+
+namespace FindAllAnagramsInAString
+{
+	class Solution
+	{
+	public:
+		std::vector<int> findAnagrams(std::string s, std::string p);
+	};
+}
--- Exercises/Main.cpp
+++ Exercises/Main.cpp
@@ -1,8 +1,10 @@
 #include "MajorityElement.h"
 #include <vector>
+#include <iostream>
 
 int main() {
 	MajorityElement::Solution s;
 	std::vector<int> nums{ 0, 1, 2, 2, 2 };
 	s.majorityElement(nums);
+	std::cout << s.majorityElement(nums) << std::endl;
 }
