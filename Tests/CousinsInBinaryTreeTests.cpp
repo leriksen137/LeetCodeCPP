a0dc352e8a7e6e3a26207e5e1d3fc67cec27ab64
Laurent Eriksen <laurent.eriksen@loepfe.com>
2020-05-17 19:58:12 +0200

    Remove all TODO and compiler warnings

--- Tests/CousinsInBinaryTreeTests.cpp
+++ Tests/CousinsInBinaryTreeTests.cpp
@@ -7,17 +7,39 @@
 TEST_CASE("Cousins in Binary Tree, Example 1", "[LeetCode]")
 {
 	Solution s;
-	REQUIRE("TODO" == "TODO");
+	TreeNode* t4 = new TreeNode(4);
+	TreeNode* t3 = new TreeNode(3);
+	TreeNode* t2 = new TreeNode(2, t4, nullptr);
+	TreeNode* root = new TreeNode(1, t2, t3);
+	int x = 4;
+	int y = 3;
+
+	REQUIRE(s.isCousins(root, x, y) == false);
 }
 
 TEST_CASE("Cousins in Binary Tree, Example 2", "[LeetCode]")
 {
 	Solution s;
-	REQUIRE("TODO" == "TODO");
+	TreeNode* t5 = new TreeNode(5);
+	TreeNode* t4 = new TreeNode(4);
+	TreeNode* t3 = new TreeNode(3, nullptr, t5);
+	TreeNode* t2 = new TreeNode(2, t4, nullptr);
+	TreeNode* root = new TreeNode(1, t2, t3);
+	int x = 5;
+	int y = 4;
+
+	REQUIRE(s.isCousins(root, x, y) == true);
 }
 
 TEST_CASE("Cousins in Binary Tree, Example 3", "[LeetCode]")
 {
 	Solution s;
-	REQUIRE("TODO" == "TODO");
+	TreeNode* t4 = new TreeNode(4);
+	TreeNode* t3 = new TreeNode(3);
+	TreeNode* t2 = new TreeNode(2, t4, nullptr);
+	TreeNode* root = new TreeNode(1, t2, t3);
+	int x = 2;
+	int y = 3;
+
+	REQUIRE(s.isCousins(root, x, y) == false);
 }
--- Tests/OddEvenLinkedListTests.cpp
+++ Tests/OddEvenLinkedListTests.cpp
@@ -2,16 +2,37 @@
 
 #include "OddEvenLinkedList.cpp"
 
+#include "ListNode.h"
+#include <vector>
+
 TEST_CASE("Odd Even Linked List, Example 1", "[LeetCode]")
 {
 	Solution s;
+	ListNode* l5 = new ListNode(5);
+	ListNode* l4 = new ListNode(4, l5);
+	ListNode* l3 = new ListNode(3, l4);
+	ListNode* l2 = new ListNode(2, l3);
+	ListNode* l1 = new ListNode(1, l2);
+
+	ListNode* result = s.oddEvenList(l1);
+	std::vector<int> expected{ 1,3,5,2,4 };
 	
-	REQUIRE("TODO" == "TODO");
+	REQUIRE(result->toVector() == expected);
 }
 
 TEST_CASE("Odd Even Linked List, Example 2", "[LeetCode]")
 {
 	Solution s;
+	ListNode* l7 = new ListNode(7);
+	ListNode* l6 = new ListNode(4, l7);
+	ListNode* l5 = new ListNode(6, l6);
+	ListNode* l4 = new ListNode(5, l5);
+	ListNode* l3 = new ListNode(3, l4);
+	ListNode* l2 = new ListNode(1, l3);
+	ListNode* l1 = new ListNode(2, l2);
 
-	REQUIRE("TODO" == "TODO");
+	ListNode* result = s.oddEvenList(l1);
+	std::vector<int> expected{ 2,3,6,7,1,5,4 };
+
+	REQUIRE(result->toVector() == expected);
 }
