4e8e9728d4eb89b6be907fbcd15776f94e17417b
Laurent Eriksen <laurent.eriksen@loepfe.com>
2020-05-20 23:41:31 +0200

    Solve the problem

--- Exercises/KthSmallestElementInABst.cpp
+++ Exercises/KthSmallestElementInABst.cpp
@@ -6,6 +6,31 @@ class Solution
 public:
 	int kthSmallest(TreeNode* root, int k)
 	{
-		return 0;
+		this->result = 0;
+		this->k = k;
+		this->count = 0;
+
+		kthSmallestRecursive(root);
+		return result;
 	}
+
+	void kthSmallestRecursive(TreeNode* root)
+	{
+		if (root == nullptr) return;
+		if (count >= k) return;
+		kthSmallestRecursive(root->left);
+		count++;
+		if (k == count)
+		{
+			result = root->val;
+			return;
+		}
+		kthSmallestRecursive(root->right);
+		return;
+
+	}
+
+	int count{ 0 };
+	int result{ 0 };
+	int k{ 0 };
 };
