208dbdd41f8ad0d54e31943d3c54c69736ee8337
Laurent Eriksen <laurent.eriksen@loepfe.com>
2020-05-29 11:36:13 +0200

    Add new test case for which solution fails

--- Exercises/CourseSchedule.cpp
+++ Exercises/CourseSchedule.cpp
@@ -1,4 +1,4 @@
-#include <vector>
+﻿#include <vector>
 
 
 class Solution
@@ -6,6 +6,31 @@ class Solution
 public:
 	bool canFinish(int numCourses, std::vector<std::vector<int>>& prerequisites)
 	{
+		while (prerequisites.size())
+		{
+			std::vector<bool> marked(numCourses, false);
+
+			std::vector<int> p = prerequisites.back();
+			prerequisites.pop_back();
+			marked[p[0]] = true;
+			std::vector<int> toVisit{ p[1] };
+			while (toVisit.size())
+			{
+				int v = toVisit.back();
+				toVisit.pop_back();
+				if (marked[v]) return false;
+				else marked[v] = true;
+				for (int i = 0; i < prerequisites.size(); i++)
+				{
+					if (prerequisites[i][0] == v)
+					{
+						toVisit.push_back(prerequisites[i][1]);
+						prerequisites.erase(prerequisites.begin() + i);
+						i--;
+					}
+				}
+			}
+		}
 		return true;
 	}
 };
