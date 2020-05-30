a6feb1dbb9519963506f97bdb7e3013c12c179da
Laurent Eriksen <laurent.eriksen@loepfe.com>
2020-05-30 10:13:41 +0200

    Improve K Closes Points solution

--- Exercises/KClosestPointstoOrigin.cpp
+++ Exercises/KClosestPointstoOrigin.cpp
@@ -7,7 +7,7 @@ class Solution
 public:
     std::vector<std::vector<int>> kClosest(std::vector<std::vector<int>>& points, int K)
     {
-        std::partial_sort(points.begin(), points.begin() + K, points.end(), [](std::vector<int>& p, std::vector<int>& q) {
+        std::partial_sort(points.begin(), points.begin() + K, points.end(), [&](std::vector<int>& p, std::vector<int>& q) {
             return p[0] * p[0] + p[1] * p[1] < q[0] * q[0] + q[1] * q[1];
             });
         return std::vector<std::vector<int>>(points.begin(), points.begin() + K);
